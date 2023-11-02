e595cf5d105338f7fa6771945d269a28d49cece8
cjfly1006 <67370596+cjfly1006@users.noreply.github.com>
2023-11-02 12:16:26 +0000

    Add files via upload

---
+++ sqlconnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LoginInterface
+{
+    class sqlconnection
+    {
+        
+
+        // Define your connection string
+        string connectionString = "Data Source=YourServer;Initial Catalog=YourDatabase;Integrated Security=True";
+
+        // Create a connection object
+        SqlConnection connection = new SqlConnection(connectionString);
+
+    }
+}
