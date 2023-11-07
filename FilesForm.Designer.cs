7accf864701ec31c813ebd2675b350b1c3393a65
Harry Giddens <HG419@student.aru.ac.uk>
2023-11-07 17:06:55 +0000

    testing

---
+++ FilesForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace LoginInterface
+{
+    partial class FilesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.FilePathTextBox = new System.Windows.Forms.TextBox();
+            this.DocumentViewer = new System.Windows.Forms.DataGridView();
+            this.ViewButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DocumentViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(469, 26);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseButton.TabIndex = 0;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(550, 26);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // FilePathTextBox
+            // 
+            this.FilePathTextBox.Location = new System.Drawing.Point(12, 27);
+            this.FilePathTextBox.Name = "FilePathTextBox";
+            this.FilePathTextBox.Size = new System.Drawing.Size(451, 22);
+            this.FilePathTextBox.TabIndex = 2;
+            // 
+            // DocumentViewer
+            // 
+            this.DocumentViewer.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DocumentViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DocumentViewer.Location = new System.Drawing.Point(12, 70);
+            this.DocumentViewer.Name = "DocumentViewer";
+            this.DocumentViewer.RowHeadersWidth = 51;
+            this.DocumentViewer.RowTemplate.Height = 24;
+            this.DocumentViewer.Size = new System.Drawing.Size(613, 368);
+            this.DocumentViewer.TabIndex = 3;
+            // 
+            // ViewButton
+            // 
+            this.ViewButton.Location = new System.Drawing.Point(642, 70);
+            this.ViewButton.Name = "ViewButton";
+            this.ViewButton.Size = new System.Drawing.Size(75, 23);
+            this.ViewButton.TabIndex = 4;
+            this.ViewButton.Text = "View";
+            this.ViewButton.UseVisualStyleBackColor = true;
+            this.ViewButton.Click += new System.EventHandler(this.ViewButton_Click);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(642, 114);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 5;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // FilesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.ViewButton);
+            this.Controls.Add(this.DocumentViewer);
+            this.Controls.Add(this.FilePathTextBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Name = "FilesForm";
+            this.Text = "FilesForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FilesForm_FormClosing);
+            this.Load += new System.EventHandler(this.FilesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DocumentViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox FilePathTextBox;
+        private System.Windows.Forms.DataGridView DocumentViewer;
+        private System.Windows.Forms.Button ViewButton;
+        private System.Windows.Forms.Button OpenButton;
+    }
+}
---
+++ FilesForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginInterface
+{
+    public partial class FilesForm : Form
+    {
+        public FilesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FilesForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilesForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                // Ensure that the form is being closed by the user (not programmatically).
+                Application.Exit();
+            }
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            FilePathTextBox.Text = ofd.FileName;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            DbConnector.GetInstanceOfDBConnector().SaveFile(FilePathTextBox.Text);
+            MessageBox.Show("Saved");
+        }
+
+        private void ViewButton_Click(object sender, EventArgs e)
+        {
+            DataTable dataTable = DbConnector.GetInstanceOfDBConnector().DisplayFileData();
+            DocumentViewer.DataSource = dataTable;
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            var selectedRow = DocumentViewer.SelectedRows;
+            foreach (var row in selectedRow)
+            {
+                int id = (int)((DataGridViewRow)row).Cells[0].Value;
+                DbConnector.GetInstanceOfDBConnector().OpenFile(id);
+            }
+        }
+    }
+}
