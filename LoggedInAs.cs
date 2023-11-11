9984392cea4e965efd7b49ba3644478972cc1664
Harry Giddens <HG419@student.aru.ac.uk>
2023-11-11 13:14:46 +0000

    more logger stuff

---
+++ LoggedInAs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginInterface
+{
+    class LoggedInAs
+    {
+        // Another singleton design to keep track of who is logged in
+        private static LoggedInAs _instance;
+        private LoggedInAs() { }
+        public string currentUserEmail
+        {
+            get;
+            set;
+        }
+        public static LoggedInAs GetInstanceOfLoggedInAs()
+        {
+            if (_instance == null)
+            {
+                _instance = new LoggedInAs();
+                return _instance;
+            }
+            return _instance;
+        }
+    }
+}
---
+++ LoggerBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginInterface
+{
+    public abstract class LoggerBase
+    {
+        // https://www.infoworld.com/article/2980677/implement-a-simple-logger-in-csharp.html
+        // Was heavily used as a reference for the making of this logger
+        public abstract void Log(string message);        
+    }
+    public class FileLogger : LoggerBase
+    {
+        public string filePath = Constants_Functions.logFilePath;
+
+        public override void Log(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+            {
+                streamWriter.WriteLine(message);
+            }
+        }
+    }
+    public class DBLogger : LoggerBase
+    {
+        string filePath = Constants_Functions.logFilePath;
+        string email = LoggedInAs.GetInstanceOfLoggedInAs().currentUserEmail;
+        public override void Log(string message)
+        {
+            DbConnector.GetInstanceOfDBConnector().SaveFile(filePath);
+        }
+    }
+}
