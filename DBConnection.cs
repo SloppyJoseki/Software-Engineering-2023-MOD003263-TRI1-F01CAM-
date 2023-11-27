d848c0bda63c801bfbcea977f9d52a53e1a86a77
v4mpily <145614494+v4mpily@users.noreply.github.com>
2023-11-27 02:35:31 +0000

    Headache1
    Ma filter broke when i tried to make it filter though everythang and now ma damn database aint workin, yall monster-truckers should give it a go w your databases or sumn, yeehaw (I am so tired)

---
+++ DBConnection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchFeature
+{
+    class DBConnection
+    {
+        private static DBConnection _instance;
+
+        private string dBConnectionString;
+
+        private DBConnection()
+        {
+            dBConnectionString = Properties.Settings.Default.DBConnectionString;
+        }
+
+        //methods
+
+        public static DBConnection getInstanceOfDBConnection()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public DataSet getDataSet(string sqlQuery, params SqlParameter[] parameters)
+        {
+            DataSet dataset = new DataSet();
+
+            try
+            {
+                using(SqlConnection connToDB = new SqlConnection(dBConnectionString))
+                {
+                    connToDB.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, connToDB))
+                    {
+                        if (parameters != null)
+                        {
+                            adapter.SelectCommand.Parameters.AddRange(parameters);
+                        }
+
+                        adapter.Fill(dataset);
+                    }
+
+                }
+
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Database Error:" + ex.Message);
+            }
+
+            return dataset;
+        }
+
+    }
+}
---
+++ SearchForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Reflection;
+
+namespace SearchFeature
+{
+    public partial class SearchForm : Form
+    {
+        private DBConnection dbConn;
+
+        public SearchForm()
+        {
+            InitializeComponent();
+            dbConn = DBConnection.getInstanceOfDBConnection();
+        }
+
+
+        private void SearchAndFilter(string searchText, string typeFilter, string cloudFilter)
+        {
+            // Construct base SQL query
+            string query = "SELECT * FROM Software WHERE ";
+
+            // Implement GetColumnNames method 
+            string[] columnNames = GetColumnNames(); 
+            for (int i = 0; i < columnNames.Length; i++)
+            {
+                if (i > 0)
+                {
+                    query += " OR ";
+                }
+                query += $"CONVERT(NVARCHAR(MAX), [{columnNames[i]}]) LIKE @searchText";
+
+            }
+
+            //Add filters based on searchbar and combobox
+            //searchtext
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                query += "CONVERT(NVARCHAR(MAX), [Software_name]) LIKE @searchText";
+            }
+
+            //typefilter
+            if (!string.IsNullOrEmpty(typeFilter))
+            {
+                if (!string.IsNullOrEmpty(searchText))
+                {
+                    query += " AND ";
+
+                }
+                query += "[Software_Type] = @typeFilter";
+            }
+
+            //cloudfilter
+            if (!string.IsNullOrEmpty(cloudFilter))
+            {
+                if (!string.IsNullOrEmpty(searchText) || !string.IsNullOrEmpty(typeFilter))
+                {
+                    query += " AND ";
+
+                }
+                query += "[Cloud] = @cloudFilter";
+            }
+
+
+            //Add parameters to query
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@SearchText", $"%{searchText}%"),
+                new SqlParameter("@typeFilter", typeFilter),
+                new SqlParameter("@cloudFilter", cloudFilter)
+
+            };
+
+
+            DataSet filteredData = dbConn.getDataSet(query, parameters);
+
+            if (filteredData != null && filteredData.Tables.Count > 0)
+            {
+                dataGridView1.DataSource = filteredData.Tables[0];
+            }
+
+            else
+
+            {
+                Console.WriteLine($"No matching data found for search: {searchText}, Type: {typeFilter}, Cloud: {cloudFilter}");
+            }
+
+        }
+
+
+        private void FillGrid() 
+        {
+            DataSet dataS = dbConn.getDataSet("SELECT * FROM Software");
+            if (dataS != null && dataS.Tables.Count > 0)
+            {
+                dataGridView1.DataSource = dataS.Tables[0];
+            }
+            else
+            {
+                Console.WriteLine("No data found in software table");
+            }
+        
+        }
+
+
+        private string[] GetColumnNames()
+        {
+            DataSet schema = dbConn.getDataSet("SELECT TOP 0 * FROM Software");
+            return schema.Tables[0].Columns.Cast<DataColumn>().Select(c => c.ColumnName).ToArray();
+
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+       
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+            string searchText = searchBar.Text.Trim();
+            string typeFilter = comboBoxType.SelectedItem?.ToString();
+            string cloudFilter = comboBoxCloud.SelectedItem?.ToString();
+            
+            SearchAndFilter(searchText, typeFilter, cloudFilter);
+           
+        }
+
+        private void searchBar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) //comboBoxType, won't let me change
+        {
+            //Starts filtering when selected item in comboboxType changes
+            buttonSearch_Click(sender, e);
+        }
+
+        private void SearchForm_Load_1(object sender, EventArgs e)
+        {
+            // Fill Datagrid with initial data at beginning
+            FillGrid();
+        }
+
+        private void comboBoxCloud_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Starts filtering when selected item in comboboxCloud changes
+            buttonSearch_Click(sender, e);
+        }
+    }
+}
