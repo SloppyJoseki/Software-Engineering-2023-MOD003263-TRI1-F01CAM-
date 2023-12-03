2683d9f749630bea3721990b7e42229d26fc13f1
Harry Giddens <HG419@student.aru.ac.uk>
2023-12-03 16:50:49 +0000

    comments innit

---
+++ Form2.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace LoginInterface
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSN = new System.Windows.Forms.TextBox();
+            this.textBoxD = new System.Windows.Forms.TextBox();
+            this.textBoxAI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSN
+            // 
+            this.textBoxSN.Location = new System.Drawing.Point(205, 98);
+            this.textBoxSN.Name = "textBoxSN";
+            this.textBoxSN.Size = new System.Drawing.Size(167, 22);
+            this.textBoxSN.TabIndex = 0;
+            // 
+            // textBoxD
+            // 
+            this.textBoxD.Location = new System.Drawing.Point(205, 148);
+            this.textBoxD.Name = "textBoxD";
+            this.textBoxD.Size = new System.Drawing.Size(167, 22);
+            this.textBoxD.TabIndex = 1;
+            // 
+            // textBoxAI
+            // 
+            this.textBoxAI.Location = new System.Drawing.Point(205, 199);
+            this.textBoxAI.Name = "textBoxAI";
+            this.textBoxAI.Size = new System.Drawing.Size(167, 22);
+            this.textBoxAI.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Software Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Software Design:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Additional Info:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAI);
+            this.Controls.Add(this.textBoxD);
+            this.Controls.Add(this.textBoxSN);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSN;
+        private System.Windows.Forms.TextBox textBoxD;
+        private System.Windows.Forms.TextBox textBoxAI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,42 @@
+﻿using SearchFeature;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginInterface
+{
+    public partial class Form2 : Form
+    {
+        public static Form2 frm;
+        public static Form2 getform2
+        {
+            get
+            {
+                if (frm == null)
+                {
+                    frm = new Form2();
+                }
+
+                return frm;
+            }
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            textBoxSN.Text = SearchForm.selectedrow.Cells[0].Value.ToString();
+            textBoxD.Text = SearchForm.selectedrow.Cells[1].Value.ToString();
+            textBoxAI.Text = SearchForm.selectedrow.Cells[2].Value.ToString();
+        }
+    }
+}
